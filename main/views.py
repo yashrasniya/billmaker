443baffcf7227aa182810f7b158b39f4679804da
yash <yashrasniya@gmail.com>
2020-10-02 23:52:49 +0530

    aysh

--- main/views.py
+++ main/views.py
@@ -6,14 +6,15 @@ from .forms import data_of_user
 # Create your views here.
 def index(request):
     number = 3
-    # number=request.POST['num']
+    number=request.POST.get('num',True)
+
     form = data_of_user(request.POST or None)
     if form.is_valid():
         form.save();
         form = data_of_user()
         return redirect('product')
     lis=[]
-    for a in range(number):
+    for a in range(int(number)):
         lis.append(a)
     context = {
         'form': form,
--- product/views.py
+++ product/views.py
@@ -3,14 +3,32 @@ from .models import product
 from .forms import product_details
 # Create your views here.
 def product(requst):
+    # number = requst.POST.get('num', True)
+
     form = product_details(requst.POST or None)
+    name = requst.POST.get('name_of_product', True)
+    print(name,"hii")
     if form.is_valid():
-        form.save();
+        form.save()
         form = product_details()
         return redirect('/')
+    if requst.POST.get('num',True) is not None:
+        number=requst.POST.get('num',True)
 
-    data={
-        'form':form,
+        lis=[]
+        for a in range(int(number)):
+            lis.append(a)
+        data = {
+            'form': form,
+            'num': lis
+        }
 
+        return render(requst,'product.html',data)
+    # lis = []
+    # for a in range(int(number)):
+    #     lis.append(a)
+    data = {
+        'form': form,
+        # 'num': lis
     }
     return render(requst,'product.html',data)
